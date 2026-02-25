efc24965e54773d8dd4ac443820168b0f30a3578
Carl-Lee91 <ckdwkdl91@gmail.com>
2026-02-25 10:20:04 +0900

    ✨ Feat: add matching page UI page

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -5,6 +5,7 @@ import { MatchingFilter } from "@/src/widgets/matching-filter";
 import { MatchingList } from "@/src/widgets/matching-list";
 import { Button } from "@/src/shared/ui/button";
 import { useUserStore } from "@/src/entities/user";
+import { ROUTES } from "@/src/shared/config/routes";
 import { Plus } from "lucide-react";
 
 export function HomePage() {
@@ -18,10 +19,7 @@ export function HomePage() {
 
       {isHost && (
         <Button
-          onClick={() => {
-            // TODO: [HOME-01] 매칭 생성 페이지로 이동
-            console.log("매칭 생성 페이지 이동");
-          }}
+          onClick={() => router.push(ROUTES.MATCHING_CREATE)}
           className="fixed bottom-20 right-4 h-14 w-14 rounded-full shadow-lg md:right-[calc(50%-215px+16px)]"
           size="icon"
         >
